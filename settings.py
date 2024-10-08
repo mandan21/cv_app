7c7e12e62b0465bfdfc019972a5833849bea8571
Amanda <amanda@pop-os.localdomain>
2024-10-08 06:24:01 +0200

    added html files, i will use for the web pages

--- settings.py
+++ settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users', 
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'cv_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -80,6 +81,8 @@ DATABASES = {
     }
 }
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
-# users/views.py
 from django.shortcuts import render, redirect
 from .forms import UserRegisterForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+import pandas as pd
+from .models import CV
+from django.core.files.storage import FileSystemStorage
 
 def register(request):
     if request.method == 'POST':
@@ -20,3 +22,19 @@ def dashboard(request):
     cv = request.user.cv if hasattr(request.user, 'cv') else None
     return render(request, 'users/dashboard.html', {'cv': cv})
 
+@login_required
+def upload_cv(request):
+    if request.method == 'POST' and request.FILES['cv']:
+        uploaded_file = request.FILES['cv']
+        fs = FileSystemStorage()
+        filename = fs.save(uploaded_file.name, uploaded_file)
+        
+        # Read the uploaded Excel file
+        file_path = fs.path(filename)
+        excel_data = pd.read_excel(file_path)
+        excel_json = excel_data.to_json()
+
+        # Save to the database
+        CV.objects.update_or_create(user=request.user, defaults={'upload': uploaded_file, 'data': excel_json})
+        return redirect('dashboard')
+    return render(request, 'users/upload.html')
