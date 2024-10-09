c239d95b02a572454e5a84d958525b45fb5ba8d3
Amanda <amanda@pop-os.localdomain>
2024-10-09 11:11:22 +0200

    added url for login

--- cv_app/urls.py
+++ cv_app/urls.py
@@ -17,9 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),  # Include user app URLs
     path('', RedirectView.as_view(url='users/register/', permanent=False)), 
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+
 ]
